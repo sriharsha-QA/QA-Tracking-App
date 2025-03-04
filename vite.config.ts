d5b13013ddca36f525f1b6042f06fcc569e6bd5d
Sriharsha <harshavardhanreddy3697@gmail.com>
2025-03-04 13:08:06 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,19 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: [".js", ".ts", ".tsx"],
+  build: {
+    target: "esnext",
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
+    },
   },
   optimizeDeps: {
     exclude: ["lucide-react"],
-  },
-  esbuild: {
-    target: "esnext",
-    loader: "tsx",
   },
 });
