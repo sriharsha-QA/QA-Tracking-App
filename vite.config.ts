cdbd585f1790e78f28c4e218ec9b936ee2e7dfa3
Sriharsha <harshavardhanreddy3697@gmail.com>
2025-03-04 12:50:39 +0530

    Fixes for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
+  },
+  esbuild: {
+    target: "esnext",
+    loader: "tsx",
   },
 });
