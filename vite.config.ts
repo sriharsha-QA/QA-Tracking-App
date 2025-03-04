130fc500265ee658c0035c1a36a99dd809824dd8
Sriharsha <harshavardhanreddy3697@gmail.com>
2025-03-04 14:45:12 +0530

    changes updated

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "./", // Ensures relative asset paths
   plugins: [react()],
   build: {
     target: "esnext",
